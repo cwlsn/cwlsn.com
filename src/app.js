029928600201cdc4b5fc97984aea9c36fd21450d
Connor Wilson <connor@axiomatic.ca>
2018-03-13 22:42:13 -0400

    start with styles

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { render } from 'react-dom'
+
+// Import styles
+import './assets/style.scss'
 
 const root = document.getElementById('root')
 
-const App = <h1>hi hello react</h1>
+const App = (
+	<Fragment>
+		<header>
+			<h1>hi hello react</h1>
+		</header>
+		<section>
+			fun section
+		</section>
+		<footer>
+			<p>feet are weird</p>
+		</footer>
+	</Fragment>
+)
 
 render(App, root)
