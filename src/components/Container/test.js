0d66831ebab23f6fa223c38156c6106e5a243fcb
Connor Wilson <connor.wilson@tulip.io>
2018-04-02 14:07:29 -0400

    add basic unit tests for not dying

--- src/components/Container/test.js
+++ src/components/Container/test.js
@@ -2,11 +2,23 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import Container from '.'
 
-test('Container', () => {
+test('Container snapshot', () => {
 	const tree = renderer
 		.create(<Container />)
 		.toJSON()
 
 	expect(tree)
 		.toMatchSnapshot()
+})
+
+test('Container renders children', () => {
+	const root = renderer
+		.create(<Container><h1>cats</h1></Container>)
+		.root
+
+	expect(root.findAllByType('h1').length)
+		.toBe(1)
+
+	expect(root.findByProps({ children: 'cats' }).type)
+		.toBe('h1')
 })
--- src/components/Currently/test.js
+++ src/components/Currently/test.js
@@ -2,11 +2,19 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import Currently from '.'
 
-test('Currently', () => {
+test('Currently snapshot', () => {
 	const tree = renderer
 		.create(<Currently />)
 		.toJSON()
 
 	expect(tree)
 		.toMatchSnapshot()
+})
+
+test('Currently renders without death', () => {
+	const tree = renderer
+		.create(<Currently />)
+		.root
+
+	expect(tree.findAllByType(Currently)).toHaveLength(1)
 })
