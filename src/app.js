6bb5414a53fc0eb2aac8e3c99d7b9c1774669b89
Connor Wilson <connor@axiomatic.ca>
2018-04-02 23:10:48 -0400

    add some fancy colours with a u

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { render } from 'react-dom'
-import { injectGlobal } from 'styled-components'
+import { injectGlobal, keyframes } from 'styled-components'
 
 import Container from './components/Container'
 import Header from './components/Header'
@@ -8,12 +8,23 @@ import Currently from './components/Currently'
 import Social from './components/Social'
 import RepoLink from './components/RepoLink'
 
+import styles from './config/styles'
+
+const getKeyframe = (color, index, array) => `${Math.floor((index / (array.length - 1)) * 100)}% {
+	background: ${color};
+}`
+
+const spectrumTime = `${styles.background.length * styles.backgroundDuration}s`
+const spectrum = keyframes`
+	${styles.background.map((color, index, original) => getKeyframe(color, index, original))}
+`
+
 injectGlobal`
 	body {
 		margin: 0;
-		font-family: 'Enriqueta', serif;
-		background: #353e4c;
+		font-family: ${styles.fontFamily};
 		color: rgba(255,255,255,.95);
+		animation: ${spectrum} ${spectrumTime} linear infinite alternate;
 	}
 `
 
---
+++ src/config/styles.js
@@ -0,0 +1,11 @@
+const styles = {
+	background: [
+		'#353e4c',
+		'#354c3a',
+		'#4c3535',
+	],
+	backgroundDuration: 7,
+	fontFamily: `'Enriqueta', serif`,
+}
+
+export default styles
